56c151c0740f72dcf785a27611c82835b3c6b34e
nerandell <ankitchandawala@gmail.com>
2015-07-14 12:47:25 +0530

    Updating version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='cauldron',
-      version='1.0.0',
+      version='1.0.1',
       author='Ankit Chandawala',
       author_email='ankitchandawala@gmail.com',
       url='https://github.com/nerandell/cauldron',
