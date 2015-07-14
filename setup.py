8f8496aec0e4d8db7dcba38c5533e8b7304200f6
nerandell <ankitchandawala@gmail.com>
2015-07-14 12:38:04 +0530

    Adding setup scripts

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='vyked',
+      version='1.0.0',
+      author='Ankit Chandawala',
+      author_email='ankitchandawala@gmail.com',
+      url='https://github.com/nearndell/cauldron',
+      description='Utils to reduce boilerplate code',
+      packages=['cauldron'], install_requires=['aiopg'])
