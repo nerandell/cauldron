778889fdf066dbccb3f140048a3980c6cb880d62
nerandell <ankitchandawala@gmail.com>
2015-07-14 12:46:47 +0530

    Fixing github link

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
-setup(name='vyked',
+setup(name='cauldron',
       version='1.0.0',
       author='Ankit Chandawala',
       author_email='ankitchandawala@gmail.com',
-      url='https://github.com/nearndell/cauldron',
+      url='https://github.com/nerandell/cauldron',
       description='Utils to reduce boilerplate code',
       packages=['cauldron'], install_requires=['aiopg'])
