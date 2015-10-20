60e2346008f8b740824eb33d1f633670f5e4579c
nerandell <ankitchandawala@gmail.com>
2015-10-20 16:33:17 +0530

    Bumping up version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cauldron',
-      version='1.0.12',
+      version='1.0.13',
       author='Ankit Chandawala',
       author_email='ankitchandawala@gmail.com',
       url='https://github.com/nerandell/cauldron',
