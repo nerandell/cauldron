d03432c447c3532ec714d5db887ea590dc007767
Aman Mathur <aman.mathur@TECH-AMANMATHUR.local>
2016-10-17 18:28:24 +0530

    Bumped version to 1.0.22 after being gifted the bug fix for incorrect key creation in Redis caching

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cauldron',
-      version='1.0.21',
+      version='1.0.22',
       author='Ankit Chandawala',
       author_email='ankitchandawala@gmail.com',
       url='https://github.com/nerandell/cauldron',
