46f9c5d469627bb60a932ee06139a373d8b9823b
Aman Mathur <aman.mathur@TECH-AMANMATHUR.local>
2017-01-10 18:25:10 +0530

    Bumped version to 1.0.25 after adding periodical pool connection cleansing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cauldron',
-      version='1.0.24',
+      version='1.0.25',
       author='Ankit Chandawala',
       author_email='ankitchandawala@gmail.com',
       url='https://github.com/nerandell/cauldron',
