b7169b8f99085730b064b084380daf37dc4d0990
Vimal <vimal.sharma@1mg.com>
2017-03-27 17:19:15 +0530

    change version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cauldron',
-      version='1.0.26',
+      version='1.0.27',
       author='Ankit Chandawala',
       author_email='ankitchandawala@gmail.com',
       url='https://github.com/nerandell/cauldron',
