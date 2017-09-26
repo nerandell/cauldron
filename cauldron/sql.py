2b3fec610af670aa024e8feb81dfc19cd6a2f205
Chirag Patel <b.like.no.other@gmail.com>
2017-09-26 17:48:48 +0530

    updated refresh_period to 30 sec

--- cauldron/sql.py
+++ cauldron/sql.py
@@ -138,7 +138,7 @@ class PostgresStore:
     @classmethod
     def connect(cls, database: str, user: str, password: str, host: str, port: int, *, use_pool: bool=True,
                 enable_ssl: bool=False, minsize=1, maxsize=30, keepalives_idle=5, keepalives_interval=4, echo=False,
-                refresh_period=30, 
+                refresh_period=0.5,
                 **kwargs):
         """
         Sets connection parameters
