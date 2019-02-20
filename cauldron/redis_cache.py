fce8abdaaf02f59a41a5c2c53be7a91fea7593fc
mayank-garg-1 <mayank.garg1@1mg.com>
2019-02-20 08:24:42 +0530

    removing logging

--- cauldron/redis_cache.py
+++ cauldron/redis_cache.py
@@ -207,7 +207,7 @@ class RedisCache:
                             new_args.append(arg)
                     _args = str(new_args)
                 redis_key = json.dumps({'func': func.__name__, 'args': _args, 'kwargs': kwargs}, sort_keys=True)
-                logger.info("AAA: args in old decorator: {}".format(_args))
+                #logger.info("AAA: args in old decorator: {}".format(redis_key))
                 digest_key = hashlib.md5(redis_key.encode(cls._utf8)).hexdigest()
                 result = yield from RedisCache.get_key(digest_key, name_space)
                 if result:
@@ -263,7 +263,7 @@ class RedisCache:
                         _kwargs[key]= kwargs[key]
 
                 redis_key = json.dumps({'func': func.__name__, 'args': _args, 'kwargs': _kwargs}, sort_keys=True)
-                logger.info("AAA: args in new decorator: {}".format(_args))
+                #logger.info("AAA: args in new decorator: {}".format(redis_key))
                 digest_key = hashlib.md5(redis_key.encode(cls._utf8)).hexdigest()
                 result = yield from RedisCache.get_key(digest_key, name_space)
                 if result:
