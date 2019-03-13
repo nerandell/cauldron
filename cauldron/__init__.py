db872f19be97d296e112013cf3b698b7ea0340d6
Rohit Taneja <rohit.or.taneja@gmail.com>
2019-03-13 15:14:43 +0530

    New Class RCedisCacheV2 added for object level redis cache connections

--- cauldron/__init__.py
+++ cauldron/__init__.py
@@ -1,5 +1,5 @@
-__all__ = ['PostgresStore', 'RedisCache', 'elasticsearch']
+__all__ = ['PostgresStore', 'RedisCache', 'elasticsearch', 'RedisCacheV2']
 
 from .sql import PostgresStore
-from .redis_cache import RedisCache
+from .redis_cache import RedisCache, RedisCacheV2
 from .es import  elasticsearch
