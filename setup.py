137cbf73d4a3acd4cbedc787f9ae3ddabddf9878
Apurva Singhal <apurva.singhal@1mg.com>
2019-04-03 17:38:30 +0530

    versioning

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cauldron',
-      version='2.1.0',
+      version='1.2.0',
       author='Ankit Chandawala',
       author_email='ankitchandawala@gmail.com',
       url='https://github.com/nerandell/cauldron',
