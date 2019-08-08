931833fa92f862f1b6a134d9449edfcd232237ed
Prashant Mishra <prashant.mishra@1mg.com>
2019-08-08 16:55:01 +0530

    Versionig done

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cauldron',
-      version='1.2.2',
+      version='1.3.0',
       author='Ankit Chandawala',
       author_email='ankitchandawala@gmail.com',
       url='https://github.com/nerandell/cauldron',
