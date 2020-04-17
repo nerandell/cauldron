6ccce7aa5f5981c8d44a983c92988cfa71bb0dbe
Sidhant Mishra <sidhant.mishra@1mg.com>
2020-04-17 17:56:33 +0530

    use of unlink to delete keys

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cauldron',
-      version='1.5.3',
+      version='1.5.4',
       author='Ankit Chandawala',
       author_email='ankitchandawala@gmail.com',
       url='https://github.com/nerandell/cauldron',
